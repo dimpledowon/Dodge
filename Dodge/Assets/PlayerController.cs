0d9505b7363478b0f7870ef5b5ba30ef15d94b0f
dimpledowon <dowon1102@naver.com>
2022-01-24 12:41:07 +0900

    20220124
    Dodge

--- Dodge/Assets/PlayerController.cs
+++ Dodge/Assets/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody playerRigidbody; // 이동에 사용할 리지드바디 컴포넌트
-    public float speed = 8f;
+    public float speed = 8f; // 이동 속력
 
     void Start()
     {
