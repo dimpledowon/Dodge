202ed251ce5f3783db3ccfcdd5a74d694fff76e2
dimpledowon <dowon1102@naver.com>
2022-02-04 12:40:26 +0900

    20220204
    Dodge

--- Dodge/Assets/BulletSpawner.cs
+++ Dodge/Assets/BulletSpawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BulletSpawner : MonoBehaviour
 {
-    public GameObject bulleyPrefab; // 생성할 탄알의 원본 프리팹
+    public GameObject bulletPrefab; // 생성할 탄알의 원본 프리팹
     public float spawnRateMin = 0.5f; // 최소 생성 주기
     public float spawnRateMax = 3f; // 최대 생성 주기
 
@@ -24,6 +24,23 @@ public class BulletSpawner : MonoBehaviour
 
     void Update()
     {
-        
+        // timeAfterSpawn 갱신
+        timeAfterSpawn += Time.deltaTime;
+
+        // 최근 생성 시점에서부터 누적된 시간이 생성 주기보다 크거나 같다면
+        if(timeAfterSpawn >= spawnRate)
+        {
+            // 누적된 시간을 리셋
+            timeAfterSpawn = 0f;
+
+            // bulletPrefab의 복제본을
+            // transform.position 위치와 transform.rotation 회전으로 생성
+            GameObject bullet
+                = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            // 생성된 bullet 게임 오브젝트의 정면 방향이 target을 향하도록 회전
+            bullet.transform.LookAt(target);
+            // 다음번 생성 간격을 spawnRateMin, spawnRateMax 사이에서 랜덤 지정
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        }
     }
 }
