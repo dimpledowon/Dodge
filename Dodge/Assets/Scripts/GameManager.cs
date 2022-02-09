741896697b67cc16d7aa0f7ac28db42b3b4f5194
dimpledowon <dowon1102@naver.com>
2022-02-09 12:46:04 +0900

    20220209
    Dodge

--- Dodge/Assets/Scripts/GameManager.cs
+++ Dodge/Assets/Scripts/GameManager.cs
@@ -22,11 +22,30 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        
+        // 게임오버가 아닌 동안
+        if(!isGameover)
+        {
+            // 생존 시간 갱신
+            surviveTime += Time.deltaTime;
+            // 갱신한 생존 시간을 timetext 텍스트 컴포넌트를 이용해 표시
+            timeText.text = "Time : " + (int)surviveTime;
+        }
+        else
+        {
+            // 게임오버 상태에서 R 키를 누른 경우
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                // SampleScene 씬을 로드
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
     }
     // 현재 게임을 게임오버 상태로 변경하는 메서드
     public void EndGame()
     {
-
+        // 현재 상태를 게임오버 상태로 전환
+        isGameover = true;
+        // 게임오버 텍스트 게임 오브젝트를 활성화
+        gameoverText.SetActive(true);
     }
 }
